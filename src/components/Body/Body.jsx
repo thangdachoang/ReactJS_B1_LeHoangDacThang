0e6d9576a6b90ef1b7609cb0864ab13b88bb2e06
thangdachoang <thang.dachoang@gmail.com>
2022-09-21 17:52:56 +0700

    push

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import Banner from './Banner'
+import Item from './Item'
 
 export default class Body extends Component {
   render() {
     return (
-      <div>Body</div>
+      <div>
+        <Banner/>
+        <Item/>
+      </div>
     )
   }
 }
